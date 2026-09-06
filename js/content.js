948d86f940acca46c52ac92556229264781c6ad0
rinaldisign <rinaldisign@gmail.com>
2026-09-06 16:45:07 +0900

    Update content.js

--- js/content.js
+++ js/content.js
@@ -64,8 +64,8 @@ export const floors = [
     name: "1F",
     image: "assets/floorplan.jpg",
     points: [
-  { target: "view1", x: 21.8, y: 37.8 },
-  { target: "view2", x: 75.6, y: 68.7 },
+  { target: "view1", x: 69.3, y: 68.3 },
+  { target: "view2", x: 37, y: 20.6 },
 ],
   },
 ];
